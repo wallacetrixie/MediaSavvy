c0fadab7811ba229b36598bf2a8bf5a059186389
wallace wambulwa <wallacewambulwa@gmail.com>
2025-04-08 11:58:07 +0300

    banner component setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Navbar from './Navbar.jsx';
+import Banner from './Banner.jsx';
 
 const App = () => {
   return (
-  <Navbar/>
+    <>
+    <Navbar/>
+    <Banner/>
+    </>
+  
   );
 };
 
